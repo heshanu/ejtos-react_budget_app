bb7f13ea2119569a1b1299a44911e6779a4a732f
heshanu <heshanu97@gmail.com>
2024-01-06 11:14:49 +0530

    comiited

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,36 @@
 
+import React, { useContext,useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget,dispatch } = useContext(AppContext);
+
+    
+    const updateBudget = () => { 
+        if (budget < 2000) {
+            dispatch({
+            type:'SET_BUDGET',
+            payload:budget,
+        })}
+    }
+    return (
+        <>
+         <div className='alert alert-secondary'>
+        <span>Budget:£ </span>
+
+        <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={budget}
+                    style={{ size: 10 }}
+                        onChange={(event) =>updateBudget(event.target.value)}
+                        />
+                        
+        </div>
+        </>
+       
+    );
+};
+
+export default Budget;
